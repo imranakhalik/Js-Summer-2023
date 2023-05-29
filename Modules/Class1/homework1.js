70263b9446faf38f8f7f75cf5186704b396434cb
Imrana Khalik <imrana@Imranas-Air.lan>
2023-05-28 22:13:19 -0400

    HomeworkClassOne

---
+++ Modules/Class1/homework1.js
@@ -0,0 +1,23 @@
+let myName = 'Imrana'; 
+console.log(`${myName}`)
+
+let myAge = 24;
+console.log(`${24}`);
+
+let isStudent = false;
+console.log(`Imrana is not a student ${isStudent}`);
+
+let favoriteColors = ['red, purple, blue'];
+console.log(`My Fav Colors are ${favoriteColors}`);
+
+let person = {
+    name: 'Sam',
+    age: 24,
+    isStudent: false,
+}
+console.log(`${person.name}`);
+console.log(`${person.age}`)
+console.log(`${person.isStudent}`)
+
+
+
